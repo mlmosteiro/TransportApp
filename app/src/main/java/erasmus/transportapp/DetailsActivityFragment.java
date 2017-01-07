d5ca19cbcc0305932e1896e1039a5fd64a0dda50
Mary Luz Mosteiro Del Pilar <marymost22@gmail.com>
2017-01-08 00:59:34 +0100

    SeekBars working RadioGroup working

--- app/src/main/java/erasmus/transportapp/DetailsActivityFragment.java
+++ app/src/main/java/erasmus/transportapp/DetailsActivityFragment.java
@@ -1,18 +1,10 @@
 package erasmus.transportapp;
 
-import android.content.res.ColorStateList;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.AppCompatActivity;
-import android.view.Gravity;
 import android.view.LayoutInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.LinearLayout.LayoutParams;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -26,19 +18,6 @@ public class DetailsActivityFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_details, container, false);
-
-
-     //TODO: Poner boton para enviar propuesta.
-
-        Button sendProposal = new Button(view.getContext());
-
-        LayoutParams params = new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
-        params.weight = 1.0f;
-        params.gravity = Gravity.END;
-        sendProposal.setText(R.string.send_n_porposal);
-        sendProposal.setTextSize(12);
-        sendProposal.setTextColor(ColorStateList.valueOf(1));
-        sendProposal.setLayoutParams(params);
 
 
         return view;
--- app/src/main/java/erasmus/transportapp/PublishActivityFragment.java
+++ app/src/main/java/erasmus/transportapp/PublishActivityFragment.java
@@ -1,6 +1,7 @@
 package erasmus.transportapp;
 
 import android.app.DatePickerDialog;
+import android.graphics.Color;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -10,6 +11,12 @@ import android.view.ViewGroup;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -18,15 +25,19 @@ import java.util.Locale;
 /**
  * A placeholder fragment containing a simple view.
  */
-public class PublishActivityFragment extends Fragment implements OnClickListener {
+public class PublishActivityFragment extends Fragment implements OnClickListener, RadioGroup.OnCheckedChangeListener {
 
     private SimpleDateFormat dateFormatter;
+    private RadioGroup radioGroup;
     private EditText loadDateET;
     private EditText downloadDateET;
     private DatePickerDialog loadDateDialog;
     private DatePickerDialog downloadDateDialog;
-    ImageButton loadDateButton;
-    ImageButton downloadDateButton;
+    private SeekBar priceBar;
+    private ImageButton loadDateButton;
+    private ImageButton downloadDateButton;
+    private TextView vehicleDetailsLabel;
+    private EditText vehicleDetails;
 
     public PublishActivityFragment() {
     }
@@ -34,24 +45,47 @@ public class PublishActivityFragment extends Fragment implements OnClickListener
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view =inflater.inflate(R.layout.fragment_publish, container, false);
+        final View view = inflater.inflate(R.layout.fragment_publish, container, false);
+
+        radioGroup = (RadioGroup) view.findViewById(R.id.rb_group);
+        radioGroup.setOnCheckedChangeListener(this);
+
+        final TextView price = (TextView) view.findViewById(R.id.tv_priceNumber);
+        priceBar = (SeekBar) view.findViewById(R.id.sb_price);
+        priceBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                price.setText("$" + Integer.toString(progress+50));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                price.setTextColor(Color.RED);
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                price.setTextColor(Color.BLACK);
+            }
+        });
 
         dateFormatter = new SimpleDateFormat("dd-MM-yyyy", Locale.getDefault());
-        loadDateET= (EditText) view.findViewById(R.id.et_loadDate);
-        downloadDateET= (EditText) view.findViewById(R.id.et_downloadDate);
-
+        loadDateET = (EditText) view.findViewById(R.id.et_loadDate);
+        downloadDateET = (EditText) view.findViewById(R.id.et_downloadDate);
         loadDateButton = (ImageButton) view.findViewById(R.id.btn_loadDate);
         downloadDateButton = (ImageButton) view.findViewById(R.id.btn_downloadDate);
         loadDateButton.setOnClickListener(this);
         downloadDateButton.setOnClickListener(this);
-
         loadDateDialog = setDataPicker(loadDateET);
         downloadDateDialog = setDataPicker(downloadDateET);
+
+        vehicleDetailsLabel = (TextView) view.findViewById(R.id.tv_vehicleDetailsLabel);
+        vehicleDetails = (EditText) view.findViewById(R.id.et_vehicleDetails);
 
         return view;
     }
 
-    private DatePickerDialog setDataPicker(final EditText fild){
+    private DatePickerDialog setDataPicker(final EditText fild) {
         Calendar newCalendar = Calendar.getInstance();
         DatePickerDialog datePickerDialog = new DatePickerDialog(getActivity(), new DatePickerDialog.OnDateSetListener() {
             @Override
@@ -66,10 +100,30 @@ public class PublishActivityFragment extends Fragment implements OnClickListener
     }
 
     @Override
-    public void onClick(View view){
-           if ( view == loadDateButton)
-                    loadDateDialog.show();
-            else if( view == downloadDateButton)
-                    downloadDateDialog.show();
-            }
+    public void onClick(View view) {
+        if (view == loadDateButton) {
+            loadDateDialog.show();
+        } else if (view == downloadDateButton) {
+            downloadDateDialog.show();
+        }
     }
+
+
+    @Override
+    public void onCheckedChanged(RadioGroup group, int checkedId) {
+        switch(checkedId)        {
+            case R.id.rb_carrier:
+                vehicleDetailsLabel.setVisibility(View.VISIBLE);
+                vehicleDetails.setVisibility(View.VISIBLE);
+                vehicleDetails.setText("");
+                break;
+            case R.id.rb_shipper:
+                vehicleDetailsLabel.setVisibility(View.GONE);
+                vehicleDetails.setVisibility(View.GONE);
+                vehicleDetails.setText("shipper");
+                break;
+        }
+
+    }
+}
+
