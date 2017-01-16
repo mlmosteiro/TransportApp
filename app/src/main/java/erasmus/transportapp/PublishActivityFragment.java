fb94c8310b0d2fcb11510107ccac0535c33aaf89
Mary Luz Mosteiro Del Pilar <marymost22@gmail.com>
2017-01-16 02:10:23 +0100

    Place picker implemented :D

--- app/src/main/java/erasmus/transportapp/PublishActivityFragment.java
+++ app/src/main/java/erasmus/transportapp/PublishActivityFragment.java
@@ -1,6 +1,7 @@
 package erasmus.transportapp;
 
 import android.app.DatePickerDialog;
+import android.content.Intent;
 import android.graphics.Color;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
@@ -17,12 +18,20 @@ import android.widget.RadioGroup;
 import android.widget.SeekBar;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlacePicker;
 
 import org.w3c.dom.Text;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Locale;
+
+import static android.app.Activity.RESULT_OK;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -38,8 +47,15 @@ public class PublishActivityFragment extends Fragment implements OnClickListener
     private SeekBar priceBar;
     private ImageButton loadDateButton;
     private ImageButton downloadDateButton;
+    private ImageButton originButton;
+    private ImageButton destinationButton;
+    private TextView originTV;
+    private TextView destinationTV;
     private TextView vehicleDetailsLabel;
     private EditText vehicleDetails;
+
+    int PLACE_PICKER_REQUEST_ORIGIN = 1;
+    int PLACE_PICKER_REQUEST_DESTINATION = 2;
 
     public PublishActivityFragment() {
     }
@@ -76,15 +92,22 @@ public class PublishActivityFragment extends Fragment implements OnClickListener
         downloadDateET = (EditText) view.findViewById(R.id.et_downloadDate);
         loadDateButton = (ImageButton) view.findViewById(R.id.btn_loadDate);
         downloadDateButton = (ImageButton) view.findViewById(R.id.btn_downloadDate);
-        loadDateButton.setOnClickListener(this);
-        downloadDateButton.setOnClickListener(this);
-        loadDateDialog = setDataPicker(loadDateET);
-        downloadDateDialog = setDataPicker(downloadDateET);
-
+        originButton = (ImageButton) view.findViewById(R.id.btn_origin);
+        destinationButton = (ImageButton) view.findViewById(R.id.btn_destination);
+        originTV = (TextView) view.findViewById(R.id.tv_originText);
+        destinationTV = (TextView) view.findViewById(R.id.tv_destinationText);
         vehicleDetailsLabel = (TextView) view.findViewById(R.id.tv_vehicleDetailsLabel);
         vehicleDetails = (EditText) view.findViewById(R.id.et_vehicleDetails);
 
         Spinner type = (Spinner) view.findViewById(R.id.spinner_type);
+
+        loadDateButton.setOnClickListener(this);
+        downloadDateButton.setOnClickListener(this);
+        loadDateDialog = setDataPicker(loadDateET);
+        downloadDateDialog = setDataPicker(downloadDateET);
+        originButton.setOnClickListener(this);
+        destinationButton.setOnClickListener(this);
+
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getActivity(),R.array.type_array, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         type.setAdapter(adapter);
@@ -112,6 +135,24 @@ public class PublishActivityFragment extends Fragment implements OnClickListener
             loadDateDialog.show();
         } else if (view == downloadDateButton) {
             downloadDateDialog.show();
+        } else if ( view == originButton) {
+            PlacePicker.IntentBuilder builder = new PlacePicker.IntentBuilder();
+            try {
+                startActivityForResult(builder.build(getActivity()), PLACE_PICKER_REQUEST_ORIGIN);
+            } catch (GooglePlayServicesRepairableException e) {
+                e.printStackTrace();
+            } catch (GooglePlayServicesNotAvailableException e) {
+                e.printStackTrace();
+            }
+        }else if ( view == destinationButton){
+            PlacePicker.IntentBuilder builder = new PlacePicker.IntentBuilder();
+            try {
+                startActivityForResult(builder.build(getActivity()), PLACE_PICKER_REQUEST_DESTINATION);
+            } catch (GooglePlayServicesRepairableException e) {
+                e.printStackTrace();
+            } catch (GooglePlayServicesNotAvailableException e) {
+                e.printStackTrace();
+            }
         }
     }
 
@@ -130,7 +171,32 @@ public class PublishActivityFragment extends Fragment implements OnClickListener
                 vehicleDetails.setText("shipper");
                 break;
         }
+    }
 
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == PLACE_PICKER_REQUEST_ORIGIN) {
+            if (resultCode == RESULT_OK) {
+                Place place = PlacePicker.getPlace(data, getActivity());
+                originTV.setText(place.getName());
+                String toastMsg = String.format("Place: %s , %s", place.getName(), place.getAddress());
+                //TODO quitar esto si no hace falta :D
+                Toast.makeText(getActivity(), toastMsg, Toast.LENGTH_LONG).show();
+
+
+            }
+        }
+        else if (requestCode == PLACE_PICKER_REQUEST_DESTINATION) {
+            if (resultCode == RESULT_OK) {
+                Place place = PlacePicker.getPlace(data, getActivity());
+                destinationTV.setText(place.getName());
+                String toastMsg = String.format("Place: %s , %s", place.getName(), place.getAddress());
+                //TODO quitar esto si no hace falta :D
+                Toast.makeText(getActivity(), toastMsg, Toast.LENGTH_LONG).show();
+
+
+            }
+        }
     }
 }
 
